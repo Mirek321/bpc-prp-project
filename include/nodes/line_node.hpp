bfa58e0176aec822d20a3faac2617584d613da12
Miroslav <mirkobasista@gmail.com>
2026-03-17 16:09:06 +0100

    add all funcionality for line following

--- include/nodes/line_node.hpp
+++ include/nodes/line_node.hpp
@@ -3,6 +3,11 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/u_int16_multi_array.hpp>
 
+#define MIN_L_VALUE 25
+#define MAX_L_VALUE 1024
+#define MIN_R_VALUE 40
+#define MAX_R_VALUE 988
+
 enum class DiscreteLinePose {
     LineOnLeft,
     LineOnRight,
@@ -25,12 +30,16 @@ namespace nodes {
 
         // Relative pose to line [m]
         float get_continuous_line_pose() const;
+        float current_continuous = 0;
 
         DiscreteLinePose get_discrete_line_pose() const;
 
     private:
         rclcpp::Subscription<std_msgs::msg::UInt16MultiArray>::SharedPtr subscriber_;
         rclcpp::Time last_process_time_;
+        float threshold_l = 0.01;
+        float threshold_r = 0.01;
+        LineSensorCalibration calibration_;
 
         void on_line_sensors_msg(const std_msgs::msg::UInt16MultiArray::SharedPtr msg);
 
--- src/nodes/line_node.cpp
+++ src/nodes/line_node.cpp
@@ -23,17 +23,44 @@ namespace nodes {
             return; // Exit instantly, doing no work
         }
         
+        uint16_t left = msg->data[0];
+        uint16_t right = msg->data[1];
+        if(left < calibration_.left_min_value) calibration_.left_min_value = left;
+        if(left > calibration_.left_max_value) calibration_.left_max_value = left;
+        if(right < calibration_.right_min_value) calibration_.right_min_value = right;
+        if(right > calibration_.right_max_value) calibration_.right_max_value = right;
+
+        current_continuous =  estimate_continuous_line_pose(left, right);
+
         // Update the tracker
         last_process_time_ = now;
-        RCLCPP_INFO(this->get_logger(), "Received line sensor data: [%u, %u]", msg->data[0], msg->data[1]);
+        // RCLCPP_INFO(this->get_logger(), "Received line sensor data: [%u, %u]", msg->data[0], msg->data[1]);
         
     }
 
     float LineNode::estimate_continuous_line_pose(float left_value, float right_value) {
-        // Implementation here
+        float calibrated_left = (left_value - (float)MIN_L_VALUE) / (MAX_L_VALUE - (float)MIN_L_VALUE);
+        float calibrated_right = (right_value - (float)MIN_R_VALUE) / (MAX_R_VALUE - (float)MIN_R_VALUE); 
+
+        RCLCPP_INFO(this->get_logger(), "Calibrated line sensor data: [%f, %f]", calibrated_left, calibrated_right);
+        return calibrated_left - calibrated_right;
     }
 
     DiscreteLinePose LineNode::estimate_discrete_line_pose(float l_norm, float r_norm) {
-        // Implementation here
+        int sensor_l = l_norm > threshold_r;
+        int sensor_r = r_norm > threshold_r;
+
+        if(sensor_l && sensor_r){
+            return DiscreteLinePose::LineBoth;
+        }
+        else if(!sensor_l && !sensor_r){
+            return DiscreteLinePose::LineNone;
+        }
+        else if(!sensor_l && sensor_r){
+            return DiscreteLinePose::LineOnLeft;
+        }
+        else if(sensor_l && !sensor_r){
+            return DiscreteLinePose::LineOnRight;
+        }
     }
 }
